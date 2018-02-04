5b4a577b20ff5736c29fb2b3d3cb9f7175e0a8fe
Pin Lin <moneycat711@gmail.com>
2018-02-05 05:59:54 +0800

    pull out variable path

--- gitea.sh
+++ gitea.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-if [ -f /var/services/homes/pinlin/tunnel_gcp/running ]; then
-    echo $$ >> /var/services/homes/pinlin/tunnel_gcp/running
+path='/var/services/homes/pinlin/tunnel_gcp'
+
+if [ -f $path/running ]; then
+    echo $$ >> $path/running
     while true; do
         sudo -u pinlin ssh -NR 10081:localhost:3000 pinlin@ntut.com.tw
         sleep 1s
--- ssh.sh
+++ ssh.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-if [ -f /var/services/homes/pinlin/tunnel_gcp/running ]; then
-    echo $$ >> /var/services/homes/pinlin/tunnel_gcp/running
+path='/var/services/homes/pinlin/tunnel_gcp'
+
+if [ -f $path/running ]; then
+    echo $$ >> $path/running
     while true; do
         sudo -u pinlin ssh -NR 9488:localhost:22 pinlin@ntut.com.tw
         sleep 1s
