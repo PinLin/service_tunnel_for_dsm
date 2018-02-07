609c4a79dcc8e7a05247bd6033b2eb11bfa986e8
Pin Lin <moneycat711@gmail.com>
2018-02-08 03:23:06 +0800

    put control to upstart

--- gitea.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-path='/var/services/homes/pinlin/tunnel_gcp'
-
-while true; do
-    sudo -u pinlin ssh -NR 10081:localhost:3000 pinlin@ntut.com.tw
-    sleep 1s
-done
--- ssh.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-path='/var/services/homes/pinlin/tunnel_gcp'
-
-while true; do
-    sudo -u pinlin ssh -NR 9488:localhost:22 pinlin@ntut.com.tw
-    sleep 1s
-done
