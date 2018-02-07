4810c13c1688d2bb3c13cc821b0a57fc9af8c624
Pin Lin <moneycat711@gmail.com>
2018-02-08 03:23:11 +0800

    recover /etc/init

--- tunnel_gcp.sh
+++ tunnel_gcp.sh
@@ -1,13 +1,21 @@
 #!/bin/sh
 
-path='/var/services/homes/pinlin/tunnel_gcp'
+path='/var/services/homes/{{user}}/tunnel_gcp'
 
 case $1 in
     start)
-	touch $path/running
-	for file in $(find $path/*.sh); do
-		$file &
-	done
+	# SSH
+	if ! [ -f /etc/init/tunnel_ssh.conf ]; then
+        cp $path/tunnel_ssh.conf /etc/init/
+        initctl reload-configuration
+        initctl start tunnel_ssh
+    fi
+	# Gitea
+	if ! [ -f /etc/init/tunnel_gitea.conf ]; then
+        cp $path/tunnel_gitea.conf /etc/init/
+        initctl reload-configuration
+        initctl start tunnel_gitea
+    fi
 	;;
     stop)
     ;;
