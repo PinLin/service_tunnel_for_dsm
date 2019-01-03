96554d3cd3356dae8948d1611829d4b1b14eec8c
Pin Lin <moneycat711@gmail.com>
2019-01-03 10:38:41 +0800

    Remove duplicate command

--- service_tunnel.sh
+++ service_tunnel.sh
@@ -14,35 +14,30 @@ case $1 in
     # DS216J Gitea SSH
     if ! [ -f /etc/init/tunnel_ds216j_gitea_ssh.conf ]; then
         sed "s={{user}}=$user=g" $path/tunnel_ds216j_gitea_ssh.conf > /etc/init/tunnel_ds216j_gitea_ssh.conf
-        cp $path/tunnel_ds216j_gitea_ssh.conf /etc/init/
         initctl reload-configuration
         initctl start tunnel_ds216j_gitea_ssh
     fi
     # DS216J Gitea Web
     if ! [ -f /etc/init/tunnel_ds216j_gitea_web.conf ]; then
         sed "s={{user}}=$user=g" $path/tunnel_ds216j_gitea_web.conf > /etc/init/tunnel_ds216j_gitea_web.conf
-        cp $path/tunnel_ds216j_gitea_web.conf /etc/init/
         initctl reload-configuration
         initctl start tunnel_ds216j_gitea_web
     fi
     # DS216J Web
     if ! [ -f /etc/init/tunnel_ds216j_web.conf ]; then
         sed "s={{user}}=$user=g" $path/tunnel_ds216j_web.conf > /etc/init/tunnel_ds216j_web.conf
-        cp $path/tunnel_ds216j_web.conf /etc/init/
         initctl reload-configuration
         initctl start tunnel_ds216j_web
     fi
     # DS216J DSM
     if ! [ -f /etc/init/tunnel_ds216j_dsm.conf ]; then
         sed "s={{user}}=$user=g" $path/tunnel_ds216j_dsm.conf > /etc/init/tunnel_ds216j_dsm.conf
-        cp $path/tunnel_ds216j_dsm.conf /etc/init/
         initctl reload-configuration
         initctl start tunnel_ds216j_dsm
     fi
     # 3770 RDP
     if ! [ -f /etc/init/tunnel_3770_rdp.conf ]; then
         sed "s={{user}}=$user=g" $path/tunnel_3770_rdp.conf > /etc/init/tunnel_3770_rdp.conf
-        cp $path/tunnel_3770_rdp.conf /etc/init/
         initctl reload-configuration
         initctl start tunnel_3770_rdp
     fi
