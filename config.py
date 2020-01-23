0debe4f0ea6ec6895afca6222b4f05163ba50faa
Pin Lin <moneycat711@gmail.com>
2020-01-23 22:22:35 +0800

    Update example config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 TUNNELS = [
     {
-        'description': "Internal MySQL",
+        'description': "PC01 MariaDB",
         'user': 'sa',
         'address': ':3306:localhost:3306',
         'destination': 'sa@example.com',
